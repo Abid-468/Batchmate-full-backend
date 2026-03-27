c2e743632ec1933630d39df820b698ee14b4b740
Arora-505 <amiarora777@gmail.com>
2026-03-27 20:32:46 +0600

    Delete routes/user_Routes.js

--- routes/user_Routes.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const {register_User,login_User,}=require("../controller/user_Controller");
-const validateToken=require("../middleware/validationTokenHandler");
-const router=express.Router();
-router.post("/register",register_User);
-router.post("/login",validateToken,login_User);
-
-
-module.exports=router;
