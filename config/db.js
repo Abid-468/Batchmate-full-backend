b94ae559ddefb6005de36283b785830f66f47e72
Md Abid Sharkar <smdabid84@gmail.com>
2026-04-16 20:09:16 +0600

    first commit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
-const mongoose=require("mongoose");
-const db=async()=>{
-    try{
-      const connect=await mongoose.connect(process.env.CONNECTION_STRING);
-      console.log("Database Connected: ",connect.connection.host,connect.connection.name);
-    }catch(err){
-        console.log(err);
-        process.exit(1);
-    }
-
+const mongoose = require("mongoose");
+const db = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      "Database Connected: ",
+      connect.connection.host,
+      connect.connection.name,
+    );
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 };
-module.exports=db;
+module.exports = db;
